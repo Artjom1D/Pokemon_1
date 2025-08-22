b1bdaac9ee610d1e4986a1a80926a381493dfb55
Loxnebot <loxnebot@gmail.com>
2025-08-22 13:58:02 +0300

    RARE

--- logic.py
+++ logic.py
@@ -4,21 +4,18 @@ import requests
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer):
+    def __init__(self, pokemon_trainer, level=1, types=None, hungry=False):
 
         self.pokemon_trainer = pokemon_trainer   
-
+        self.types = []
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
         self.type = self.get_type()
         self.abilities = self.get_abilities()
-
+        self.level = level
+        self.hungry = hungry
         Pokemon.pokemons[pokemon_trainer] = self
-
-    # Метод для получения картинки покемона через API
-    def get_img(self):
-        pass
     
     # Метод для получения имени покемона через API
     def get_name(self):
@@ -38,14 +35,14 @@ class Pokemon:
             return (picture['sprites']['other']['official-artwork']['front_default'])
         else:
             return "сегодня нет картинок ))"
-    def get_type(self):
+    def get_types(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return data['types'][0]['type']['name']
+            return [t['type']['name'] for t in data['types']]
         else:
-            return "unknown"
+            return []
 
     def get_abilities(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -55,6 +52,32 @@ class Pokemon:
             return [a['ability']['name'] for a in data['abilities']]
         else:
             return []
+
+    def feed(self):
+        if self.hungry:
+            return f"Ваш покемон {self.name} сыт!"
+        else:    
+            self.hungry = False
+            return f"Ваш покемон {self.name} накормлен!"
+    
+    def play(self):
+        if self.hungry == True:
+            return f"Ваш покемон {self.name} голоден и не хочет играть!"
+        else:
+            self.hungry = True
+            self.level += 1
+            return f"Ваш покемон {self.name} играет и проголодался!"
+
+    def rarity(self):
+        if self.types > 1 or self.types != "water" or "fire" or "wind" or "earth":
+            self.level += 3
+            return "Редкий покемон. Ваш уровень повышен на 3!"
+        else:
+            return "Обычный покемон. Неудача, но в следующий раз повезет!"
+
+
+    def level_up(self):
+        return f"Уровень вашего покемона {self.name} повышен до {self.level}!"
     # Метод класса для получения информации
     def info(self):
         return (f"Имя твоего покемона: {self.name}\n"
@@ -65,7 +88,7 @@ class Pokemon:
         return self.img
     
     def type_pokemon(self):
-        return f"Тип вашего покемона{self.type}"
+        return f"Тип вашего покемона: {self.type}"
 
 
 
--- main.py
+++ main.py
@@ -13,9 +13,29 @@ def go(message):
         bot.send_message(message.chat.id, pokemon.type_pokemon())
         bot.send_message(message.chat.id, "Вот твой покемон!")
         bot.send_photo(message.chat.id, pokemon.show_img())
+        bot.send_message(message.chat.id, pokemon.rarity())
+        bot.send_message(message.chat.id, "Покормить покемона - /feed\n"
+                                      "Поиграть с покемоном - /play\n"
+                                      "Узнать уровень покемона - /levelup\n")
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
+@bot.message_handler(commands=["feed"])
+def feed(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        bot.send-message(message.chat.id, "Сначала создай покемона командой /go")
+    else:
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.feed())
+
+
+@bot.message_handler(commands=['levelup'])
+def lvlup(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        bot.send_message(message.chat.id, "Сначала создай покемона командой /go")
+    else:
+        pokemon = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pokemon.level_up())
 
 bot.infinity_polling(none_stop=True)
 
