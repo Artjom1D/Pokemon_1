43bd5252be12cdec403bad7e7cbed8673a54d811
Loxnebot <loxnebot@gmail.com>
2025-08-27 15:54:55 +0300

    dsa

--- logic.py
+++ logic.py
@@ -13,10 +13,12 @@ class Pokemon:
         self.img = self.get_img()
         self.name = self.get_name()
         self.abilities = self.get_abilities()
+        self.hp = randint(10,100)
+        self.power = randint(2,10)
         self.level = 0
         self.hungry = hungry
         Pokemon.pokemons[pokemon_trainer] = self
-    
+
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -82,22 +84,73 @@ class Pokemon:
             self.level += 3
             return "Редкий покемон. Ваш уровень повышен на 3!"
         else:
-            return "Обычный покемон. Неудача, но в следующий раз повезет!"
-
+            return "Обычный покемон. Неудача, но в следующий раз повезет!"        
 
     def level_up(self):
         return f"Уровень вашего покемона {self.name} {self.level}!"
     # Метод класса для получения информации
     def info(self):
         return (f"Имя твоего покемона: {self.name}\n"
-                f"Способности: {', '.join(self.abilities)}")
-
+                f"Способности: {', '.join(self.abilities)}\n"
+                f"Тип покемона: {', '.join(self.types)}\n"
+                f"HP: {self.hp}\n"
+                f"Attack: {self.attack}\n"
+                f"Уровень: {self.level}\n"
+                f"Голоден: {'Да' if self.hungry == True else 'Нет'}\n"
+                f"{self.rarity()}\n"
+                f"{self.img}"
+                f"Класс покемона: {self.class_name()}\n")
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
     
     def type_pokemon(self):
         return f"Тип вашего покемона: {self.type}"
+    
+    def class_name(self):
+        chance = randint(1,5)
+        if chance == 1:
+            return Warrior(self.pokemon_trainer)
+        elif chance == 2: 
+            return Wizard(self.pokemon_trainer)
+        else:
+            return Pokemon(self.pokemon_trainer)
+        
+    def hospital(self):
+        self.hp += 10
+        return f"Покемон {self.name} вылечен в больнице! Его HP увеличен на 10."
 
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            chance = randint(1,5)
+            if chance == 1:
+                return f"Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            self.level += 2
+            self.hungry = True
+            self.power += 1
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+        
+class Warrior(Pokemon):    
+    def init__(self, pokemon_trainer):
+        super().__init__(pokemon_trainer)
+        self.power += 6
 
+    def attack(self, enemy):
+        супер_сила = randint(5,15)
+        self.power += супер_сила
+        результат = super().attack(enemy)
+        self.power -= супер_сила
+        return результат + f"\nБоец применил супер-атаку силой:{супер_сила} "
 
+class Wizard(Pokemon):
+    def init__(self, pokemon_trainer):
+        super().__init__(pokemon_trainer)
+        self.hp += 20
+
+    def attack(self, enemy):
+        return super().attack(enemy)
--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import telebot 
 from config import token
-
-from logic import Pokemon
+from random import randint
+from logic import Pokemon, Warrior, Wizard
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
-        bot.send_message(message.chat.id, pokemon.type_pokemon())
-        bot.send_message(message.chat.id, pokemon.rarity())
-        bot.send_message(message.chat.id, "Покормить покемона - /feed\n"
-                                      "Поиграть с покемоном - /play\n"
-                                      "Узнать уровень покемона - /level\n")
-        bot.send_photo(message.chat.id, pokemon.show_img())
         bot.send_message(message.chat.id, "Вот твой покемон!")
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
@@ -43,6 +39,27 @@ def lvlup(message):
     else:
         pokemon = Pokemon.pokemons[message.from_user.username]
         bot.send_message(message.chat.id, pokemon.level_up())
+
+@bot.message_handler(commands=['hospital'])
+def hospital(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        bot.send_message(message.chat.id, "Сначала создай покемона командой /go")
+    else:
+        pokemon = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pokemon.hospital())
+
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
 
 bot.infinity_polling(none_stop=True)
 
