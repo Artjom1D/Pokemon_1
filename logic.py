ce76116b0c0de3cf50a3020bba5a10ec02d4917e
Loxnebot <loxnebot@gmail.com>
2025-08-30 19:15:43 +0300

    feed

--- logic.py
+++ logic.py
@@ -1,10 +1,11 @@
 from random import randint
+from datetime import timedelta, datetime
 import requests
 
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer, level=None, types=None, hungry=False):
+    def __init__(self, pokemon_trainer, level=None, types=None, hungry=False, last_feed_time=None):
 
         self.pokemon_trainer = pokemon_trainer   
         self.pokemon_number = randint(1,1000)
@@ -17,6 +18,8 @@ class Pokemon:
         self.power = randint(2,10)
         self.level = 0
         self.hungry = hungry
+        self.last_feed_time = last_feed_time if last_feed_time else datetime.now()
+        
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения имени покемона через API
@@ -67,15 +70,24 @@ class Pokemon:
         else:
             return []
 
-    def feed(self):
-        if self.hungry == False:
-            return f"Ваш покемон {self.name} сыт!"
-        else:    
+    def feed(self, feed_interval = 20, hp_increase = 10):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        elif self.hungry == True:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
             self.hungry = False
-            return f"Ваш покемон {self.name} накормлен!"
-    
-    def play(self):
-        if self.hungry == True:
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {self.last_feed_time+delta_time}"
+
+
+    def play(self, feed_interval=20):
+        if self.hungry == True or self.last_feed_time + timedelta(seconds=feed_interval) < datetime.now():
             return f"Ваш покемон {self.name} голоден и не хочет играть!"
         else:
             self.hungry = True
@@ -139,6 +151,9 @@ class Warrior(Pokemon):
         результат = super().attack(enemy)
         self.power -= супер_сила
         return результат + f"\nБоец применил супер-атаку силой:{супер_сила} "
+    
+    def feed(self, feed_interval=10, hp_increase=10):
+        super().feed(feed_interval, hp_increase)
 
 class Wizard(Pokemon):
     def __init__(self, pokemon_trainer):
@@ -147,3 +162,6 @@ class Wizard(Pokemon):
 
     def attack(self, enemy):
         return super().attack(enemy)
+    
+    def feed(self, feed_interval=20, hp_increase=20):
+        super().feed(feed_interval, hp_increase)
